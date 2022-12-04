6d89d18f40545f8a0d6c2cf4c50505424a79c29b
kareemrahmanzai <111786444+kareemrahmanzai@users.noreply.github.com>
2022-12-04 14:07:03 -0600

    new

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -125,8 +125,17 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = 'crm/home.html'
 LOGOUT_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+#EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'isqatest4@gmail.com'
+EMAIL_HOST_PASSWORD = 'nublvadvbptffcls'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+SERVER_EMAIL = 'Password Reset'
+
 
 
 
